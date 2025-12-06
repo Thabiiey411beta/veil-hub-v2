94fa03a1d9e30482ab6f1e98078e412bb8dbe21c
Thabiiey <thabangzulu94@gmail.com>
2025-12-06 06:36:11 +0000

    Your descriptive commit message here

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ['lucide-react'],
-  },
+
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
