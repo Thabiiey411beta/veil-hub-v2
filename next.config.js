dd845a3d8cf5097f4ea3568c8e9bb06dea29d625
Thabiiey <thabangzulu94@gmail.com>
2025-12-06 07:12:55 +0000

    Your descriptive commit message here

--- next.config.js
+++ next.config.js
@@ -1,24 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  images: {
-    formats: ['image/webp', 'image/avif'],
-  },
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    
-    if (!isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'react/jsx-runtime.js': 'react/jsx-runtime',
-        'react/jsx-dev-runtime.js': 'react/jsx-dev-runtime',
-      };
-    }
-    
     return config;
   },
   poweredByHeader: false,
