15cb5394aab24d8b030e2130122f6823e6ae7fca
thabangzulu89-sketch <thabangzulu89@gmail.com>
2025-12-23 14:36:29 +0000

    chore(types): add minimal supra-l1-sdk declarations

--- types/supra-l1-sdk.d.ts
+++ types/supra-l1-sdk.d.ts
@@ -1,2 +1,16 @@
-declare module 'supra-l1-sdk';
+declare module 'supra-l1-sdk' {
+	export type SupraAccount = any;
+	export type HexString = string;
+	export const BCS: {
+		bcsSerializeUint64: (v: bigint) => any;
+	};
+
+	export class SupraClient {
+		static init(url: string): Promise<SupraClient>;
+		getAccountSupraCoinBalance(address: string): Promise<bigint>;
+		sendTx(account: SupraAccount, payload: any): Promise<any>;
+	}
+
+	export { SupraClient };
+}
 declare module 'supra-l1-sdk';
